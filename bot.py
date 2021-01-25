f5cee42b206145c2452774815af26601502f2e2e
itsmarti123 <77275010+itsmarti123@users.noreply.github.com>
2021-01-25 15:13:34 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+import discord
+import os
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Bruh im Ready')
+  
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+      if message.content.startswith('.help'):
+        embedVar = discord.Embed(title="Help", description="Bruh Bot Help", color=0xff0000)
+        embedVar.add_field(name=".cat - Shows you some cute cats, .invite - Sends you an invite link for the server, .vote vote this server", value="Help", inline=False)
+        await message.channel.send(embed=embedVar)
+        
+      await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=".help"))
+      
+client.run('Nzk3MzY1ODgxNzA1MjAxNjc0.X_la2A.MZZ2_a1_fQhs_3TYN02rK1w5_D0')
