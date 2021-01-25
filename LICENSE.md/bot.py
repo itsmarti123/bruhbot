e13ff21d9b7ba6210e00276335c2dd8ef25a5079
itsmarti123 <77275010+itsmarti123@users.noreply.github.com>
2021-01-25 14:45:57 +0800

    Update bot.py

--- LICENSE.md/bot.py
+++ LICENSE.md/bot.py
@@ -16,3 +16,7 @@ async def on_message():
     embedVar = discord.Embed(title="Help", description="Bruh Help", color=0xff0000)
     embedVar.add_field(name=".Vote - Vote this Discord Server on top.gg, .invite - sends you an invite link to invite more people to this server", value="Help", inline=False)
     await message.channel.send(embed=embedVar)
+    
+   await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=".help"))
+
+client.run('Nzk3MzY1ODgxNzA1MjAxNjc0.X_la2A.MZZ2_a1_fQhs_3TYN02rK1w5_D0')
