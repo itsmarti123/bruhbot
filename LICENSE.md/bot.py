14299d0caaef66c1811b0ce3e4cbf0e408bb9364
itsmarti123 <77275010+itsmarti123@users.noreply.github.com>
2021-01-25 14:42:45 +0800

    Update bot.py

--- LICENSE.md/bot.py
+++ LICENSE.md/bot.py
@@ -1,1 +1,18 @@
+import discord
+import os
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Bruh im Ready')
+  
+@client.event
+async def on_message():
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('.help'):
+    embedVar = discord.Embed(title="Help", description="Bruh Help", color=0xff0000)
+    embedVar.add_field(name=".Vote - Vote this Discord Server on top.gg, .invite - sends you an invite link to invite more people to this server", value="Help", inline=False)
+    await message.channel.send(embed=embedVar)
