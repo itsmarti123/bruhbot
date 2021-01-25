6670610a44a1df4c8f6fcb23d0ff2f583549d624
itsmarti123 <77275010+itsmarti123@users.noreply.github.com>
2021-01-25 15:20:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,10 @@ async def on_message(message):
   if message.author == client.user:
     return
   
-      if message.content.startswith('.help'):
-        embedVar = discord.Embed(title="Help", description="Bruh Bot Help", color=0xff0000)
-        embedVar.add_field(name=".cat - Shows you some cute cats, .invite - Sends you an invite link for the server, .vote vote this server", value="Help", inline=False)
-        await message.channel.send(embed=embedVar)
+   if message.content.startswith('.help'):
+     embedVar = discord.Embed(title="Help", description="Bruh Bot Help", color=0xff0000)
+     embedVar.add_field(name=".cat - Shows you some cute cats, .invite - Sends you an invite link for the server, .vote vote this server", value="Help", inline=False)
+     await message.channel.send(embed=embedVar)
         
       await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=".help"))
       
